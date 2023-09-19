511d9fcfaa6dd62880a454d47c5621bbf0e6b46d
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-09-19 17:12:38 -0600

    delete redundant code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,7 +15,7 @@ router.get('/api/notes', (req, res) => {
   getNotes().then(notes => res.json(notes))
 });
 
-// POST /api/notes receives a new note to save on the request body, 
+// POST a new note to save on the request body, 
 router.post('/api/notes', (req, res) => {
   getNotes().then(oldNotes => {
     // creating body for note
@@ -31,10 +31,10 @@ router.post('/api/notes', (req, res) => {
     writeFile('db/db.json', JSON.stringify(noteArray)).then(() => res.json({ msg: 'Okay' }))
 
   })
-  // pushing created note to be written in the db.json file
-  // res.json(db);
+
 
 });
+// delete old notes
 router.delete('/api/notes/:id', (req, res) => {
   getNotes().then(oldNotes => {
     const noteArray = oldNotes.filter(note => note.id !== req.params.id)
@@ -45,20 +45,9 @@ router.delete('/api/notes/:id', (req, res) => {
     writeFile('db/db.json', JSON.stringify(noteArray)).then(() => res.json({ msg: 'Okay' }))
 
   })
-  // pushing created note to be written in the db.json file
-  // res.json(db);
+
 
 });
 
-// // DELETE /api/notes/:id receives a query parameter containing the id of a note to delete.
-// app.delete('/api/notes/:id', (req, res) => {
-//   // reading notes form db.json
-//   let db = JSON.parse(fs.readFileSync('db/db.json'))
-//   // removing note with id
-//   let deleteNotes = db.filter(item => item.id !== req.params.id);
-//   // Rewriting note to db.json
-//   fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-//   res.json(deleteNotes);
 
-// })
 module.exports = router
